e3bb355dca1c9bb72c14e6b805a6463acbbf6fd2
samarthmali13 <111837487+samarthmali13@users.noreply.github.com>
2023-01-12 17:05:49 +0530

    jan 12,

--- src/features/movies/MovieCard.js
+++ src/features/movies/MovieCard.js
@@ -5,9 +5,9 @@ const MovieCard = (props) => {
     const { data } = props
     //    console.log(data)
     return (
-        <div className='cus-card'>
+        <div className=''>
             <Link to={`/movie/${data.imdbID}`}>
-                <div className='card my-4' style={{'width':'18rem'}}>
+                <div className='card m-4' >
                 <img className='card-img-top ' src={data.Poster} alt={data.Title} style={{ 'height': '300px' }}/> 
                     <div className='card-body'> 
                     <div className='card-title'>{data.Title}</div>
--- src/features/movies/MovieDetail.js
+++ src/features/movies/MovieDetail.js
@@ -7,7 +7,9 @@ const MovieDetail = () => {
     const { imdbID } = useParams();
     const dispatch = useDispatch();
     const data = useSelector(getSelectedMovieOrShow);
-    console.log(data)
+    console.log()
+    const ratings = data.Ratings
+    console.log(ratings)
     useEffect(() => {
         dispatch(fetchAsyncMovieOrShowDetail(imdbID));
         return () => {
@@ -16,7 +18,7 @@ const MovieDetail = () => {
     }, [dispatch, imdbID]);
     return (
         <>
-            <div>
+            {data.Response === 'True' ? (<div className='App'>
                 <h2>{data.Title}</h2>
                 <img src={data.Poster} alt={data.Title} />
                 <div><h3>Actors :</h3> {data.Actors} </div>
@@ -25,8 +27,19 @@ const MovieDetail = () => {
                 <div><h3>Director:</h3> {data.Director}</div>
                 <div><h3>Genre:</h3> {data.Genre}</div>
                 <div><h3>Plot:</h3> {data.Plot}</div>
+                <div><h3>Awards:</h3> {data.Awards}</div>
+                <div><h3>Language:</h3> {data.Language}</div>
+                <div><h3>Rated:</h3> {data.Rated}</div>
+                <div><h3>Writer:</h3> {data.Writer}</div>
+                <div><h3>Ratings:</h3> 
+                <div>{data.Ratings[0].Source} : {data.Ratings[0].Value}</div>
+                <div>{data.Ratings[1].Source} : {data.Ratings[1].Value}</div>
+                <div>{data.Ratings[2].Source} : {data.Ratings[2].Value}</div>
 
-            </div>
+                
+                 </div>
+
+            </div>) : <div><h1>loading....</h1></div>}
         </>
     )
 }
